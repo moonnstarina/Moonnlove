cd451a7920d790ff7c51e5df802052650450cc49
mon <mon@momon.qzz.io>
2026-07-22 05:04:37 +0000

    fix: remove cloud_firestore import from home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import '../providers/theme_provider.dart';
 import '../services/auth_service.dart';
 
